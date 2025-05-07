0147f9933d87508c1caa30098380ba155dd6c031
konnn04 <trieukon1011@gmail.com>
2025-05-07 16:08:16 +0700

    edit CI/CD configuration

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,6 +2,9 @@
 cd /home/ec2-user/rent-house-app/rent_house_server
 python3 -m venv venv
 source venv/bin/activate
+yum install -y mariadb-devel gcc python3-devel
 pip install -r requirements.txt
+
+
 python manage.py makemigrations rent_house
 python manage.py migrate
